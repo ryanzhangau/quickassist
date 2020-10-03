141329a08cc93be5456dbf12e5ec28721a12ffad
ryanzhangau <ryanzhangau@gmail.com>
2020-10-04 09:01:53 +1100

    refact: update config

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,9 @@
+/** @format */
+
 import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import AnctechButton, { ButtonProps } from '../packages/button/src'
 
@@ -8,18 +12,31 @@ export default {
   component: AnctechButton,
   argTypes: {
     label: { control: 'text' },
-    fontColor: { control: 'color' },
-    backgroundColor: { control: 'color' },
-    size: { control: { type: 'select', options: ['tiny', 'small', 'medium', 'large'] } },
-    shape: {
-      control: { type: 'select', options: ['round', 'square', 'circle', 'round-conner'] }
-    }
+    size: { control: { type: 'select', options: ['sm', 'md', 'lg'] } },
+    color: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']
+      }
+    },
+    disabled: { control: 'boolean' }
   }
 } as Meta
 
-const Template: Story<ButtonProps> = args => <AnctechButton {...args} />
+const NormalButton: Story<ButtonProps> = args => <AnctechButton {...args} />
 
-export const Normal = Template.bind({})
+export const Normal = NormalButton.bind({})
 Normal.args = {
-  label: 'Button'
+  label: 'Button',
+  size: 'md',
+  color: 'primary'
+}
+
+const HollowButton: Story<ButtonProps> = args => <AnctechButton {...args} outline={true} />
+
+export const Hollow = HollowButton.bind({})
+Hollow.args = {
+  label: 'Button',
+  size: 'md',
+  color: 'primary'
 }
