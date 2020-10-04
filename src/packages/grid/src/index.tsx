f22441d7bad5255adff6683f37d4751581df6ac2
ryanzhangau <ryanzhangau@gmail.com>
2020-10-04 17:56:19 +1100

    refactor: update index import / export

--- src/packages/grid/src/index.tsx
+++ src/packages/grid/src/index.tsx
@@ -1,11 +1,8 @@
 import AnctechRow from './row'
 import AnctechCols from './col'
-import type { RowProps } from './row'
-import type { ColsProps } from './col'
 export type ScreenSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'auto'
 export type VAlignment = 'start' | 'center' | 'end'
 export type IDictionary<T> = {
   [k in ScreenSize]?: T
 }
 export { AnctechRow, AnctechCols }
-export type { RowProps, ColsProps }
